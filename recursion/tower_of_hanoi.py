321f3de7ac2b1e47ed6b8af884326cf33270fcbb
Chris Kim <cj3kim@gmail.com>
2015-06-12 16:34:06 -0700

    Guy who wrote this implementation of hanoi is super smart

--- recursion/tower_of_hanoi.py
+++ recursion/tower_of_hanoi.py
@@ -1,5 +1,5 @@
 
-start = [x for x in reversed(range(4))];
+start = [x for x in reversed(range(3))];
 start.pop()
 temp  = []
 dest  = []
@@ -9,32 +9,34 @@ def moveDisk(start_pole, temp_pole, dest_pole):
     print 'start: {0}'.format(start)
     print 'temp: {0}'.format(temp)
     print 'dest: {0}'.format(dest)
-
-
-
     disk = start_pole.pop(0)
-
-    if len(start_pole) > 0:
+    if len(start_pole) == 0:
+        dest_pole.append(disk)
+    else:
         moveDisk(start_pole, dest_pole, temp_pole)
         dest_pole.append(disk);
         moveDisk(temp_pole, start_pole, dest_pole)
-    else:
-        dest_pole.append(disk)
-
 
 moveDisk(start, temp, dest)
+
+
 print '\n'
 print 'end'
 print 'start: {0}'.format(start)
 print 'temp: {0}'.format(temp)
 print 'dest: {0}'.format(dest)
 
-#FUNCTION MoveTower(disk, source, dest, spare):
-#IF disk == 0, THEN:
-    #move disk from source to dest
-#ELSE:
-    #MoveTower(disk - 1, source, spare, dest)   // Step 1 above
-    #move disk from source to dest              // Step 2 above
-    #MoveTower(disk - 1, spare, dest, source)   // Step 3 above
-#END IF
+#http://stackoverflow.com/questions/27581683/trying-to-implement-recursive-tower-of-hanoi-algorithm-with-arrays
+def hanoi(pegs, start, target, n):
+    assert len(pegs[start]) >= n, 'not enough disks on peg'
+    if n == 1:
+        pegs[target].append(pegs[start].pop())
+        print '%i -> %i: %s' % (start, target, pegs)
+    else:
+        aux = 3 - start - target  # start + target + aux = 3
+        hanoi(pegs, start, aux, n-1)
+        hanoi(pegs, start, target, 1)
+        hanoi(pegs, aux, target, n-1)
+
+hanoi([[4,3,2,1], [], []],0, 2, 4)
 
