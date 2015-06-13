56333e29442aefd99c84cc75ed6ece26d4f36627
Chris Kim <cj3kim@gmail.com>
2015-06-12 18:15:32 -0700

    optimized with deque

--- recursion/reverse_ary.py
+++ recursion/reverse_ary.py
@@ -1,15 +1,18 @@
+from collections import deque
 
 def reverse_ary(ary):
     last_value = ary.pop()
     if len(ary) > 0: reverse_ary(ary)
-    ary.insert(0,last_value)
+    ary.appendleft(last_value)
 
 ary = range(10)
-mem_address_old = hex(id(ary))
+d = deque(ary)
 
-reverse_ary(ary)
-mem_address_new = hex(id(ary))
+print 'd: {0}'.format(d)
+mem_address_old = hex(id(d))
+reverse_ary(d)
+mem_address_new = hex(id(d))
 
-print 'ary: {0}'.format(ary)
+print 'ary: {0}'.format(d)
 print 'mem_address_old: {0}'.format(mem_address_old)
 print 'mem_address_new: {0}'.format(mem_address_new)
