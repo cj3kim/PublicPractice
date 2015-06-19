ec9bff1706340e581b02b1990ece94fa17dc1bc1
Chris Kim <cj3kim@gmail.com>
2015-06-19 15:54:07 -0700

    Added comments to one_queue_two_stack.py

--- general/stacks/one_queue_two_stacks.py
+++ general/stacks/one_queue_two_stacks.py
@@ -1,7 +1,5 @@
 from collections import deque
 
-
-
 class Queue():
     def __init__(self):
         self.stack_a = deque([])
@@ -11,30 +9,35 @@ class Queue():
         self.stack_a.append(x)
 
     def dequeue(self):
-        len_stack_b = len(self.stack_b)
-        if (len_stack_b > 0):
-            return self.stack_b.pop();
-        else:
-            len_stack_a = len(self.stack_a)
-            i = 0
-            while (len_stack_a > i ):
-                if (len_stack_a - 1) != i:
-                    self.stack_b.append(self.stack_a.pop())
-                else:
-                    return self.stack_a.pop();
+        if (len(self.stack_b) > 0): return self.stack_b.pop();
 
-                i += 1
-
-
+        i = 0
+        len_stack_a = len(self.stack_a)
+        last_index_stack_a = len_stack_a - 1
+        while (len_stack_a > i ):
+            poppedValue = self.stack_a.pop()
+            if i == last_index_stack_a: return poppedValue
+            self.stack_b.append(poppedValue)
+            i += 1
 
 def test_case():
     q = Queue()
     for i in range(10):
         q.enqueue(i)
-
+    print """
+        This algorith is O(n) utilizing the accounting method to compute the average time an
+        element spends in the system.
+    """
+    print """
+        Stack A is in the IN stack and Stack B is the OUT stack. We accumulate new element on
+        Stack A and then pop them onto Stack B when Stack B is empty and needs more elements to dequeue.
+    """
+    print """
+        It's important to note Stack A keeps the elements in reversed order due to stack properties,
+        but the order will be correct when the reversed elements are popped onto Stack B.
+    """
     print 'stack_a', q.stack_a
     print 'stack_b', q.stack_b
-
     q.dequeue()
 
     print 'stack_a', q.stack_a
