f8be32e3ba3a963526da538c27676e120a0a406b
Chris Kim <cj3kim@gmail.com>
2015-06-27 15:24:44 -0700

    #init_change passes

--- make_change/tests/make_change_test.py
+++ make_change/tests/make_change_test.py
@@ -2,10 +2,10 @@ import sys, os
 sys.path.insert(0, os.getcwd())
 from make_change import give_change, init_change
 
-def test_give_change():
-    coins = [25, 10, 5, 1]
-    dollars = [100, 50, 20, 10, 5, 1]
+coins   = [25, 10, 5, 1]
+dollars = [100, 50, 20, 10, 5, 1]
 
+def test_give_change():
     cents = 93
     change_1 = give_change(cents, coins)
 
@@ -14,6 +14,13 @@ def test_give_change():
     bucks = 135
     change_2 = give_change(bucks, dollars)
     assert change_2 == {100: 1, 50: 0 , 20: 1, 10: 1, 5: 1, 1: 0 }
+
+
+def test_init_change():
+    "Initializes a hash of currency_units with counters set to 0"
+    assert init_change(coins)   == {25: 0, 10:0, 5: 0, 1: 0}
+    assert init_change(dollars) == {100: 0, 50:0, 20: 0, 10:0, 5: 0, 1: 0}
+
 
 
 
