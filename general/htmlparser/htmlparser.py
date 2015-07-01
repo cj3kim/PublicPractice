2042405cee59d949e4d3ac98026e65526e06a14e
Chris Kim <cj3kim@gmail.com>
2015-07-01 01:42:27 -0700

    Added a test for failure in htmlparser

--- general/htmlparser/htmlparser.py
+++ general/htmlparser/htmlparser.py
@@ -6,6 +6,7 @@ class HtmlParser():
     def __init__():
         "None"
 
+#Checks for balance of html tags
 def parse(file_path):
     html_file = open(file_path, 'r')
     stack = deque()
@@ -14,20 +15,20 @@ def parse(file_path):
     right_angle_index = 0
     end_tag = False
 
-    for line in html_file:
-        for i, char in enumerate(line):
-            if char == "<":
-                end_tag_check = line[i+1] == "/"
-                left_angle_index = i+1 if end_tag_check else i
-                end_tag = True if end_tag_check else False
-            elif char == ">":
-                right_angle_index = i
+    very_long_string = html_file.read()
+    for i, char in enumerate(very_long_string):
+        if char == "<":
+            end_tag_check = very_long_string[i+1] == "/"
+            left_angle_index = i+1 if end_tag_check else i
+            end_tag = True if end_tag_check else False
+        elif char == ">":
+            right_angle_index = i
 
-                tag_name = line[left_angle_index+1: right_angle_index]
-                if end_tag:
-                    if stack[-1] == tag_name: stack.pop()
-                    end_tag = False
-                else: stack.append(tag_name)
+            tag_name = very_long_string[left_angle_index+1: right_angle_index]
+            if end_tag:
+                if stack[-1] == tag_name: stack.pop()
+                end_tag = False
+            else: stack.append(tag_name)
 
     return True if len(stack) == 0 else False
 
@@ -35,6 +36,7 @@ def parse(file_path):
 
 def test_parse():
     assert parse(os.path.join(os.getcwd(),'text.html')) == True
+    assert parse(os.path.join(os.getcwd(),'error_text.html')) == False
 
 
 
